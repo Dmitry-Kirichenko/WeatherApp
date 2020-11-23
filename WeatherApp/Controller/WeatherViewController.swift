98f043733214c1bca7d62257e99ae4fd2535a325
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-11-23 15:25:49 +0300

    Implement UI

--- WeatherApp/Controller/WeatherViewController.swift
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  WeatherApp
 //
 //  Created by Дмитрий Кириченко on 23.11.2020.
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
