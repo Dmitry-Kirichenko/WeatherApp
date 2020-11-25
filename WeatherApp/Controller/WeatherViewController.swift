c184e5822997bbc044cbb61dba57bbe5396e3c1b
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-11-25 12:24:27 +0300

    Add plist location description Update WeatherVC

--- WeatherApp/Controller/WeatherViewController.swift
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
+class WeatherViewController: UIViewController {
 
     @IBOutlet var conditionImageView: UIImageView!
     @IBOutlet var cityLabel: UILabel!
@@ -15,16 +16,32 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+       
         searchTextField.delegate = self
+        weatherManager.delegate = self
     }
+    
+    @IBAction func locationPredded(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
+}
 
-    @IBAction func searchPressed(_ sender: UIButton) {
+
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
+    @IBAction func searchPressed() {
         searchTextField.endEditing(true)
-        print(searchTextField.text!)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -45,8 +62,41 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         if let city = searchTextField.text {
             weatherManager.fetchWeather(cytyName: city)
         }
-
         searchTextField.text = ""
     }
 }
 
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
---
+++ WeatherApp/Model/WeatherModel.swift
@@ -0,0 +1,39 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Дмитрий Кириченко on 24.11.2020.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.0f", temperature)
+    }
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud"
+        default:
+            return "cloud"
+        }
+    }
+}
